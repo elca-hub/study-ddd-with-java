95787f255a0334902b902528360047651cf75084
elca-hub <csubaru07@gmail.com>
2023-01-28 11:57:13 +0900

    ロギングツールの試し

--- school/src/main/java/com/studydddwithjava/school/SchoolController.java
+++ school/src/main/java/com/studydddwithjava/school/SchoolController.java
@@ -1,5 +1,7 @@
 package com.studydddwithjava.school;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SchoolController {
     @GetMapping("/")
     public String index(Model model) {
+        Logger logger = LoggerFactory.getLogger(SchoolController.class);
+        logger.info("test!");
         return "index";
     }
 }
