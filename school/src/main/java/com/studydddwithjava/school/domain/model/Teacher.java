a385b77333c5a0c083bbcf9771d583eb03061955
elca-hub <csubaru07@gmail.com>
2023-01-31 18:32:57 +0900

    教員モデルにidを追加し、nameのデータ型をUserNameに変更

--- school/src/main/java/com/studydddwithjava/school/domain/model/Teacher.java
+++ school/src/main/java/com/studydddwithjava/school/domain/model/Teacher.java
@@ -1,11 +1,19 @@
 package com.studydddwithjava.school.domain.model;
 
 public class Teacher {
-    private String name;
+    private final int MIN_PW_LEN = 16;
+    private final int MAX_PW_LEN = 32;
+
+    private final String id;
+    private UserName name;
     private String pw;
 
-    public Teacher(String name, String pw) {
+    public Teacher(String id, UserName name, String pw) {
+        this.id = id;
         this.name = name;
+        if (pw.length() < MIN_PW_LEN || pw.length() > MAX_PW_LEN) {
+            throw new IllegalArgumentException(String.format("Password length must be between %d and %d characters.", MIN_PW_LEN, MAX_PW_LEN));
+        }
         this.pw = pw;
     }
 
