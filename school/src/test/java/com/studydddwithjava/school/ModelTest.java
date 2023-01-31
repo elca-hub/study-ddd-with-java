77fed4a171768341b9e3ce579cda5ca06d624e34
elca-hub <csubaru07@gmail.com>
2023-01-31 18:33:27 +0900

    テストケースの修正

--- school/src/test/java/com/studydddwithjava/school/ModelTest.java
+++ school/src/test/java/com/studydddwithjava/school/ModelTest.java
@@ -3,6 +3,7 @@ package com.studydddwithjava.school;
 import com.studydddwithjava.school.domain.model.Student;
 import com.studydddwithjava.school.domain.model.Task;
 import com.studydddwithjava.school.domain.model.Teacher;
+import com.studydddwithjava.school.domain.model.UserName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,17 +13,16 @@ public class ModelTest {
     @Test
     @DisplayName("教員モデルを用いて生徒モデルにタスクを付与")
     void testTeacherControl() throws Exception {
-        Teacher teacher = new Teacher("elca", "password");
+        UserName teacherName = new UserName("Domain-Driven", "Design");
+        Teacher teacher = new Teacher("teacher-id", teacherName, "passwordpassword");
 
-        Student[] students = {
-                new Student("test1", 3, 1),
-                new Student("test2", 3, 2)
-        };
+        UserName studentName = new UserName("ado", "asmi");
+        Student student = new Student("student-id", studentName, 3, 1);
 
         Task testTask = new Task("Come to 3F3", "I know you broke all window of the school. I'll talk with you.");
 
-        teacher.call(students[0], testTask);
+        teacher.call(student, testTask);
 
-        assertThat(students[0].getTasks().get(0), is(testTask));
+        assertThat(student.getTasks().get(0), is(testTask));
     }
 }
