1a786ccafc033e85bad842b3e90389e9243d3c41
elca-hub <csubaru07@gmail.com>
2023-02-04 11:21:50 +0900

    エラー文の修正

--- school/src/main/java/com/studydddwithjava/school/domain/model/group/GroupName.java
+++ school/src/main/java/com/studydddwithjava/school/domain/model/group/GroupName.java
@@ -7,7 +7,7 @@ public class GroupName {
 
     public GroupName(String name) {
         if (name.length() == 0 || name.length() > MAX_GROUP_NAME_LEN) {
-            throw new IllegalArgumentException(String.format("Password length must be between %d and %d characters.", 0, MAX_GROUP_NAME_LEN));
+            throw new IllegalArgumentException(String.format("Group name length must be between %d and %d characters.", 0, MAX_GROUP_NAME_LEN));
         }
         this.name = name;
     }
