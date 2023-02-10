c1183a96e06e422b251945e5429d47d8a7696d3d
elca-hub <csubaru07@gmail.com>
2023-02-11 08:51:37 +0900

    教員が入力した名前とパスワードの取得

--- school/src/main/java/com/studydddwithjava/school/SchoolController.java
+++ school/src/main/java/com/studydddwithjava/school/SchoolController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SchoolController {
@@ -12,6 +14,12 @@ public class SchoolController {
     public String index(Model model) {
         Logger logger = LoggerFactory.getLogger(SchoolController.class);
         logger.info("test!");
+        return "index";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("teacherName") String name, @RequestParam("teacherPw") String pw) {
+        
         return "index";
     }
 }
