ed8b182c307ab3fdbf694cd4d125c34eeab30736
elca-hub <csubaru07@gmail.com>
2023-02-12 17:39:25 +0900

    名前の下限と上限を設定

--- school/src/main/java/com/studydddwithjava/school/domain/model/user/UserName.java
+++ school/src/main/java/com/studydddwithjava/school/domain/model/user/UserName.java
@@ -4,9 +4,24 @@ public class UserName {
     private final String firstName;
     private final String lastName;
 
+    private final int MIN_NAME_LEN = 1;
+    private final int MAX_NAME_LEN = 20;
+
     public UserName(String firstName, String lastName) {
+        if (firstName.length() < MIN_NAME_LEN || firstName.length() > MAX_NAME_LEN) {
+            throw new IllegalArgumentException(String.format("First name length must be between %d and %d characters.", MIN_NAME_LEN, MAX_NAME_LEN));
+        }
+
+        if (lastName.length() < MIN_NAME_LEN || lastName.length() > MAX_NAME_LEN) {
+            throw new IllegalArgumentException(String.format("Last name length must be between %d and %d characters.", MIN_NAME_LEN, MAX_NAME_LEN));
+        }
+
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return this.firstName + this.lastName;
     }
 
     public String getFirstName() { return firstName; }
