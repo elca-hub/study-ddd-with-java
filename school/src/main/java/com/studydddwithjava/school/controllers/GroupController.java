bfb7aecafa3dc91faba794ba85da75684e7333e1
elca-hub <csubaru07@gmail.com>
2023-02-17 14:26:02 +0900

    余分な箇所の削除

--- school/src/main/java/com/studydddwithjava/school/controllers/GroupController.java
+++ school/src/main/java/com/studydddwithjava/school/controllers/GroupController.java
@@ -2,17 +2,12 @@ package com.studydddwithjava.school.controllers;
 
 import com.studydddwithjava.school.application.shared.ILogger;
 import com.studydddwithjava.school.application.teacher.TeacherApplicationService;
-import com.studydddwithjava.school.application.teacher.TeacherData;
-import com.studydddwithjava.school.infrastructure.security.LoginTeacherDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/auth/group")
@@ -25,10 +20,7 @@ public class GroupController {
     private ILogger logger;
 
     @GetMapping("/new")
-    public String index(
-            @AuthenticationPrincipal LoginTeacherDetails teacherDetails,
-            Model model
-    ) {
+    public String index(Model model) {
         return "/auth/group/new";
     }
 }
