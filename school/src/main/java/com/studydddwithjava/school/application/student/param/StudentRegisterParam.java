f74e101bb21e065d0ad5c5696ca68a4fa46084b4
elca-hub <csubaru07@gmail.com>
2023-02-18 17:49:49 +0900

    formの値を取得

---
+++ school/src/main/java/com/studydddwithjava/school/application/student/param/StudentRegisterParam.java
@@ -0,0 +1,25 @@
+package com.studydddwithjava.school.application.student.param;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+public class StudentRegisterParam {
+    @NotBlank
+    private String firstname;
+
+    @NotBlank
+    private String lastname;
+
+    @NotBlank
+    private String groupId;
+
+    @Positive
+    @Min(1)
+    private int studentNumber;
+
+    private boolean autoInc;
+}
--- school/src/main/java/com/studydddwithjava/school/controllers/StudentController.java
+++ school/src/main/java/com/studydddwithjava/school/controllers/StudentController.java
@@ -1,5 +1,6 @@
 package com.studydddwithjava.school.controllers;
 
+import com.studydddwithjava.school.application.student.param.StudentRegisterParam;
 import com.studydddwithjava.school.application.team.TeamApplicationService;
 import com.studydddwithjava.school.application.shared.ILogger;
 import com.studydddwithjava.school.application.team.TeamData;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,8 +33,19 @@ public class StudentController {
     ) {
         List<TeamData> teams = teamApplicationService.findByTeacher(teacherDetails.getUsername());
 
+        /* TODO: 出席番号をデフォルトでセットするための値の取得 */
         model.addAttribute("teams", teams);
 
         return "/auth/student/new";
+    }
+
+    @PostMapping("/register")
+    public String register(
+            @ModelAttribute @Validated StudentRegisterParam studentRegisterParam,
+            BindingResult result
+    ) {
+        if (result.hasErrors()) return "redirect:/auth/student/new?error";
+
+        return "redirect:/auth/";
     }
 }
