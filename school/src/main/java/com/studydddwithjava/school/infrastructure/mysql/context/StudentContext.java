1dec6aecee652a29a2ecf7806b2fe24c634c5570
elca-hub <csubaru07@gmail.com>
2023-03-04 18:39:51 +0900

    生徒のidをIN句で取得するメソッドの追加

--- school/src/main/java/com/studydddwithjava/school/infrastructure/mysql/context/StudentContext.java
+++ school/src/main/java/com/studydddwithjava/school/infrastructure/mysql/context/StudentContext.java
@@ -2,12 +2,11 @@ package com.studydddwithjava.school.infrastructure.mysql.context;
 
 import com.studydddwithjava.school.infrastructure.mysql.entity.StudentDataModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface StudentContext extends JpaRepository<StudentDataModel, String> {
+    List<StudentDataModel> findByIdIn(List<String> studentIds);
 }
