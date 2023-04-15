abf5ecc9f8c84e0df20302f17712108dd9e4d12f
elca-hub <csubaru07@gmail.com>
2023-04-16 08:55:56 +0900

    チームリポジトリのメソッドにコメントを追加

--- school/src/main/java/com/studydddwithjava/school/domain/model/team/ITeamRepository.java
+++ school/src/main/java/com/studydddwithjava/school/domain/model/team/ITeamRepository.java
@@ -7,13 +7,44 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ITeamRepository {
+    /**
+     * チームをDBに保存
+     * @param teacher 保存する教員モデル
+     * @param team 保存対象のチームモデル
+     */
     void save(Teacher teacher, Team team);
 
+    /**
+     * 教員モデルが保有しているチームモデルを検索
+     * @param teacher 検索対象の教員モデル
+     * @return 検索結果のリスト
+     */
     List<Team> findByTeacher(Teacher teacher);
+
+    /**
+     * チームidに対応するチームを検索
+     * @param id チームid
+     * @return 対応するチームモデル
+     */
     Optional<Team> findById(String id);
 
+    /**
+     * 生徒モデルが保有しているチームidを全て削除
+     * @param student 脱退対象の生徒モデル
+     */
+
     void removeStudent(Student student);
+
+    /**
+     * チームモデルをDBから削除
+     * @param team 削除対象のチームモデル
+     */
     void delete(Team team);
 
+    /**
+     * 生徒が所属しているチームモデルを検索
+     * @param student 検索対象の生徒モデル
+     * @return 生徒が所属しているチームモデルのリスト
+     */
     List<Team> findByStudent(Student student);
 }
